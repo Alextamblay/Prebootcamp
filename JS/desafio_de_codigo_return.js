07548925b04ec2206906cff533db0a26c5adf1cc
alextamblay <alexandratamblayiccp@gmail.com>
2022-08-26 10:04:44 -0400

    archivo

--- JS/desafio_de_codigo_return.js
+++ JS/desafio_de_codigo_return.js
@@ -94,9 +94,74 @@ console.log('result is', result);
 
 /* 
 
-// console.log ()
+// console.log (num is, num) 6
+console.log (result is, result) 16
 
 diagrama de t 
 variables          valores
-y 
-result
+y                | 6
+result           | 16
+*/
+
+function sumNums(num1, num2) {  
+    return num1+num2;
+ }
+ console.log(sumNums(2,3))
+ console.log(sumNums(3,5))
+
+ /*
+
+ //console.log(sumNums) 5
+ console.log (sunNums) 8
+ */
+
+ function printSumNums(num1, num2) {
+    console.log(num1);   
+    return num1+num2;
+ }
+ console.log(printSumNums(2,3))
+ console.log(printSumNums(3,5))
+
+ /* 
+ //console.log(num1) 2
+
+console.log(printSunNums) 5
+console.log(printSunNums) 8
+*/
+
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,5);
+console.log('result is', result);
+
+/*
+//console.log(sum is num) 5 + 8
+console.log (result is result) 13
+
+diagrama de t 
+variables         valores
+sum            |    5+8=13
+result         |    13
+
+*/
+
+function sumNums(num1, num2) {
+    var sum = num1 + num2;
+    console.log('sum is', sum);
+    return sum;
+}
+var result = sumNums(2,3) + sumNums(3,sumNums(2,1)) + sumNums(sumNums(2,1),sumNums(2,3));
+console.log('result is', result);
+
+/*
+
+//console.log (sum is 5) 
+console.log (result is result )19
+
+diagrama de t
+variables       valores
+sum        |  5 + 3 + 3 + 3 + 5
+result     | 19
