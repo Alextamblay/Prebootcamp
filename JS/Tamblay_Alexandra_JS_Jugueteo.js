f4357992dfa906fecbb385ee7c741b9ed78e8e8e
alextamblay <alexandratamblayiccp@gmail.com>
2022-09-09 11:03:18 -0400

    subir

--- JS/Tamblay_Alexandra_JS_Jugueteo.js
+++ JS/Tamblay_Alexandra_JS_Jugueteo.js
@@ -1,8 +1,10 @@
-var alturaNiño= 100
-function muestraSiElNiñoPuedeSubirALaMontañaRusa
-  if(alturaNiño>52) {
-    console.log(Súbete, chico)
-  }
-  else {
-    console.log(Lo siento, chico. Tal vez el próximo año)
-  }
+var alturaNiño= 100;
+
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño){
+
+  if(alturaNiño>52){
+    console.log("¡Súbete, chico!")
+  }else {
+    console.log("Lo siento, chico. Tal vez el próximo año")
+        }
+}
